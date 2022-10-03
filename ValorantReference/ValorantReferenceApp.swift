88cfa2777a1aeb6a5e44dc2fb451b46818558314
Marcos Chevis <marcoschevism@gmail.com>
2022-10-03 01:15:48 -0300

    Initial Commit

---
+++ ValorantReference/ValorantReferenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValorantReferenceApp.swift
+//  ValorantReference
+//
+//  Created by Marcos Chevis on 03/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ValorantReferenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
